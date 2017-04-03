67cfccb25d4f851486ff650bc44be6a95f91f0cb
Pitt009 <gmartinez28837@gmail.com>
2017-04-03 17:01:39 -0500

    comenzando twitter

---
+++ assets/js/twitter.js
@@ -0,0 +1,8 @@
+function agregarComentario(){
+  var comentario = document.getElementById('comentario').value;
+  var autor = document.getElementById('autor').value;
+  //var mostrarComentario = document.getElementById('mostrarComentario');
+  //mostrarComentario.innerHTML = comentario + "<br>"+ autor;
+  var visualizar = document.getElementById('visualizar');
+  var articulo = document.createElement("article")
+}
