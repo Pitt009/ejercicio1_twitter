0f35a281b16413bfc209de269061ba4d63494fa4
Pitt009 <gmartinez28837@gmail.com>
2017-04-04 10:26:49 -0500

    funcionando casi twitter

--- assets/js/twitter.js
+++ assets/js/twitter.js
@@ -4,18 +4,22 @@ function agregarComentario(){
   //var mostrarComentario = document.getElementById('mostrarComentario');
   //mostrarComentario.innerHTML = comentario + "<br>"+ autor;
   var visualizar = document.getElementById('visualizar');
-  var articulo = document.createElement("article")
+  var articulo = document.createElement("article");
+  var rayita = document.createElement("hr");
 
   visualizar.appendChild(articulo).innerHTML = comentario + "<br/>" + autor;
+  visualizar.appendChild(rayita);
 }
 
-/*function contar(){
+function contar(){
   var maxCaracteres = "140";
-  var comentario = document.getElementById('comentario');
+  var comentario = document.getElementById('comentario').value;
   var longitud = comentario.length;
   if(comentario <=140){
-    document.getElementById('contador')
-  }
+    document.getElementById('contador').innerHTML = maxCaracteres-longitud;
 
+  }else {
+     maxCaracteres.substr(0,maxCaracteres);
   }
-*/
+  console.log(comentario)
+}
