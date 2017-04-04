49223b655809748ca1c6e9590d5a77c2267cb465
Pitt009 <gmartinez28837@gmail.com>
2017-04-04 08:57:43 -0500

    Casi Terminado

--- assets/js/twitter.js
+++ assets/js/twitter.js
@@ -5,4 +5,17 @@ function agregarComentario(){
   //mostrarComentario.innerHTML = comentario + "<br>"+ autor;
   var visualizar = document.getElementById('visualizar');
   var articulo = document.createElement("article")
+
+  visualizar.appendChild(articulo).innerHTML = comentario + "<br/>" + autor;
 }
+
+/*function contar(){
+  var maxCaracteres = "140";
+  var comentario = document.getElementById('comentario');
+  var longitud = comentario.length;
+  if(comentario <=140){
+    document.getElementById('contador')
+  }
+
+  }
+*/
